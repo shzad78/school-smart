5523bbd8c58bb30c0fe5d78fbfbcdba91bec086a
shzad78 <shzad78@gmail.com>
2025-01-21 14:30:43 +0100

    header

---
+++ app/(front)/page.tsx
@@ -0,0 +1,11 @@
+import SiteHeader from "@/components/site-header";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <SiteHeader />
+      <h2>HomePage</h2>
+    </div>
+  );
+}
---
+++ components/logo.tsx
@@ -0,0 +1,19 @@
+import { GraduationCap } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <Link href={"/"} className="flex items-center space-x-2">
+      <div className="bg-blue-500 rounded-full p-1">
+        <span className="text-white font-bold text-xl">
+          {" "}
+          <GraduationCap />{" "}
+        </span>
+      </div>
+      <span className="font-bold text-xl">
+        Smart <span className="text-blue-500">School</span>{" "}
+      </span>
+    </Link>
+  );
+}
